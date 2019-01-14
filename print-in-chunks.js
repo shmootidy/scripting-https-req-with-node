251d6a8971def84e8a2f67b2af7296c009cc9acb
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 19:30:19 +0000

    Successfully completed Step 1. My function is printing HTML in big 'ol ugly chunks. I've also added a note on the results.

--- print-in-chunks.js
+++ print-in-chunks.js
@@ -11,7 +11,7 @@ function getAndPrintHTMLChunks() {
     response.setEncoding('utf8');
 
     response.on('data', function (data) {
-      console.log('Chunk Received. Length:', data.length);
+      console.log('Chunk:', data + '\n');
     });
 
     response.on('end', function() {
@@ -27,3 +27,4 @@ getAndPrintHTMLChunks();
 // considerations:
 // - https request should be outside the function, although I suspect it doesn't matter in this case. In most cases, my guess is it does matter, so I want to get into the habit of putting it first. That way, the module is available as a global scope.
 // - The stream function - data - will be the one that logs the data chunks as they come in. The others - 'error', 'end', and 'finish' - have different purposes.
+// - The results come in as raw HTML, including the head. This is not a great way of receiving data as it's ugly. Plus, the images are just huge strings of letters and numbers. To improve this, we may ... I'm not sure. Request the URL from a browser, which can display the HTML nicely? Hmmm...
