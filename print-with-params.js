39253dfa4568c8b6066817a39e3900c6aebdd023
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 19:57:49 +0000

    started writing the function and specifying the request options.

--- print-with-params.js
+++ print-with-params.js
@@ -1,1 +1,10 @@
 var https = require('https');
+
+function getAndPrintHTML (options) {
+  // body...
+}
+
+var requestOptions = {
+  host: "sytantris.github.io",
+  path: '/http-examples/step3.html'
+};
