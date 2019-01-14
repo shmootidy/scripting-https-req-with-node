8f4403a80aa4ea694faaaf38cd2f8707ab25a9c8
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 20:45:30 +0000

    Added some answers to the considerations.

--- http-functions.js
+++ http-functions.js
@@ -1,3 +1,5 @@
+var https = require('https');
+
 module.exports = function getHTML (options, callback) {
    https.get(options, function(response){
 
@@ -18,3 +20,7 @@ module.exports = function getHTML (options, callback) {
     })
   })
 };
+
+//considerations:
+// - My immediate impulse is to have the require('https') variable within the http-function.js file. This allows it to be private (which is always nice; keep your hands off my stuff!) but...
+// - the hint suggests we may have use for requiring httP (without the s). However, the options we are given are to put it at the top of the file (global and private) or within the function (local and not private). Functionally, I can't see much difference. Perhaps I could include another param in the getHTML function that allows for different types of requests (i.e., https or http or ft, whatever), that can be set by the file that actually calls the function. I think... Maybe...
