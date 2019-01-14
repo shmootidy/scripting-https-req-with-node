02b5c72789d6b1a1f17ff531e148d9d67030cd99
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 19:18:05 +0000

    added some notes as preliminary responses to the considerations

--- print-in-chunks.js
+++ print-in-chunks.js
@@ -8,3 +8,7 @@ function getAndPrintHTMLChunks() {
   };
 
 }
+
+// considerations:
+// - https request should be outside the function, although I suspect it doesn't matter in this case. In most cases, my guess is it does matter, so I want to get into the habit of putting it first. That way, the module is available as a global scope.
+// - The stream function - data - will be the one that logs the data chunks as they come in. The others - 'error', 'end', and 'finish' - have different purposes.
