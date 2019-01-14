ef12bbc89cbe4f31d5f4d61385d3bd376b036b00
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 21:35:33 +0000

    Solved the 1337 problem! One thing was to put the strings before the single letters, so they would actually be converted. The other was to drop the i tage (that was in the SO answer), because it was throwing errors. I'm not sure why...

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -9,18 +9,20 @@ var leetObj = {
   a: '4',
   e: '3',
   l: '1',
+  o: '0',
   s: '5',
   t: '7',
   'ck': 'x',
   'er': '0r',
   'you': 'j00'
 };
-
+//a|e|l|s|t|
 function print1337 (html) {
-  // html = html.replace(/a|e|l|s|t/gi|[ck]|[er]|[you]/gi, function(matched){
-  //   return leetObj[matched];
-  // });
-  console.log(html.replace(/[a]/g, "4").replace(/[e]/g, "3").replace(/[o]/g, "0").replace(/[l]/g, "1"));
+  html = html.replace(/ck|er|you|a|e|l|o|s|t/g, function(matched){
+    return leetObj[matched];
+  });
+  console.log(html);
+  // console.log(html.replace(/[a]/g, "4").replace(/[e]/g, "3").replace(/[o]/g, "0").replace(/[l]/g, "1"));
 }
 
 getHTML(requestOptions, print1337);
