6b1330af62aa559581942c583b71df503e28d1f2
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 20:05:55 +0000

    added a consideration thought, with my reasoning for why the requestOptions variable is now in the global scope.

--- print-with-params.js
+++ print-with-params.js
@@ -8,3 +8,6 @@ var requestOptions = {
   host: "sytantris.github.io",
   path: '/http-examples/step3.html'
 };
+
+//considerations:
+// - the requestOptions variable is a global scope so that it can be passed through the get request when the function is called. If it were local, I couldn't specify. But globally, I can have many different request option variables and call the function using them to access the relevant data.
