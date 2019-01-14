c313661b58fd87ef8b4ee6457e8ee663e481320d
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 21:05:41 +0000

    Added some of the meat to the first file - uppercase.

--- callbacks/print_uppercase.js
+++ callbacks/print_uppercase.js
@@ -1,1 +1,12 @@
 var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function printHTML (html) {
+  console.log(html);
+}
+
+getHTML(requestOptions, printHTML);
