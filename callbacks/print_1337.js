a54195af1b550844b2e35e8ddcf0a9119296b904
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 21:22:09 +0000

    ALMOST got it. The replacement of two letters is screwing with things a bit. When I took them out, the code works. Onward we go!

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -5,7 +5,21 @@ var requestOptions = {
   path: '/http-examples/step6/1337.html'
 };
 
+var leetObj = {
+  a: '4',
+  e: '3',
+  l: '1',
+  s: '5',
+  t: '7',
+  'ck': 'x',
+  'er': '0r',
+  'you': 'j00'
+};
+
 function print1337 (html) {
+  html = html.replace(/a|e|l|s|t/gi, function(matched){
+    return leetObj[matched];
+  });
   console.log(html.replace(/[a]/g, "4").replace(/[e]/g, "3").replace(/[o]/g, "0").replace(/[l]/g, "1"));
 }
 
