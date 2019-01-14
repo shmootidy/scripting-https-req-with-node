4b6df2422d918ff6d2f9babc1d1a9271b8c4572b
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 19:53:52 +0000

    Finished Step 2. Discovered that I need to console.log the concatenated data within the end handler function. I tried console.logging the concatenated string in the data handler function, but ended up with that string printed as many times as there were chunks (which makes sense). I tried returning the concat from within the data hand-func, but that didn't return diddly squat. This means that the end hand-func must have a script that accesses whatever is generated in the on hand-func... That is my reasoning. We shall see.

--- print-with-buff-strings.js
+++ print-with-buff-strings.js
@@ -4,7 +4,23 @@ function getAndPrintHTML () {
  var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step2.html'
- };
+  };
 
+  https.get(requestOptions, function (response){
+    response.setEncoding('utf8');
 
+    var dataReceived = '';
+    response.on('data', function (data) {
+      dataReceived += data;
+    });
+
+    response.on('end', function(){
+      console.log(dataReceived);
+      console.log("The end.");
+    })
+  });
 }
+
+getAndPrintHTML();
+// considerations:
+// The best output for this is probably a big ol' string. So, that's what I'm going to set as the output data.
