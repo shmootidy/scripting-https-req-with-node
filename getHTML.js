0f31308128c18d9d91e8ef0fc354e866befcd983
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 20:22:48 +0000

    Added the two functions as described in the assignment. Added a callback in the getHTML function. We shall see if it works...

--- getHTML.js
+++ getHTML.js
@@ -1,2 +1,16 @@
 var https = require('https');
 
+function getHTML (options, callback) {
+  callback();
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'syntantris.github.io',
+  path: '/http-examples/step1.html'
+};
+
+getHTML(requestOptions, printHTML);
