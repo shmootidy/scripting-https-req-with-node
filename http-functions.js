5213a7a7a6c0a5bfe6d663714b2cb73b31cda594
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 20:40:11 +0000

    copy-pasted contents of my getHTML function from my getHTML.js file.

--- http-functions.js
+++ http-functions.js
@@ -1,3 +1,20 @@
 module.exports = function getHTML (options, callback) {
-  // body...
+   https.get(options, function(response){
+
+    if(response.statusCode !== 200){
+      console.log("Whoops!");
+      return;
+    }
+
+    response.setEncoding('utf8');
+
+    var receivedHTML = '';
+    response.on('data', function(data){
+      receivedHTML += data;
+    });
+
+    response.on('end', function(){
+      callback(receivedHTML);
+    })
+  })
 };
