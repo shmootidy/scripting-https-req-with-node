3b683ba7efee8cbb79967f28ac8aaf5824aafc9a
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 21:17:53 +0000

    Sort of solved the 1337 problem by using my password refactor code that uses regex. It's not complete, though, because this question asked for more strings to be replaced. I found a solution on SO that will make my code more readable and easier to edit. https://stackoverflow.com/questions/15604140/replace-multiple-strings-with-multiple-other-strings

--- callbacks/print_1337.js
+++ callbacks/print_1337.js
@@ -1,1 +1,12 @@
 var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/1337.html'
+};
+
+function print1337 (html) {
+  console.log(html.replace(/[a]/g, "4").replace(/[e]/g, "3").replace(/[o]/g, "0").replace(/[l]/g, "1"));
+}
+
+getHTML(requestOptions, print1337);
